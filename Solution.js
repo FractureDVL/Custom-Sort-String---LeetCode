c6272cd47a87664296f53a655924e5794de8d79f
FractureDVL <joseandrescram1005@gmail.com>
2022-10-27 17:10:25 -0500

    Created repo and unfinished possible solution js and java

---
+++ Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
+var customSortString = function (order, s) {
+    
+    var o = s.split('');
+    var res = s.split('');
+
+    for (let i = 0; i < order.length; i++) {
+        for (let j = 0; j < s.length; j++) {
+        }
+    }
+    
+    return console.log(res)
+};
+
+customSortString("cbad","abcd");
