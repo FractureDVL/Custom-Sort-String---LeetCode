6e40b028d578c36b672640b5d56273719f0999e3
unknown <joseandrescram1005@gmail.com>
2022-10-28 18:55:59 -0500

    Fixed generate no-repeated characters in order string

--- Java/Gen.java
+++ Java/Gen.java
@@ -1,63 +1,71 @@
 package Java;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 // Main class
 public class Gen {
-
+    // Generate 100 cases with order and s
     public String genData() {
         String res = "";
-
         int cases = 100;
-        int orLength = (int) genRandomNum(1, 26);
+        int orLength = (int) genRandomNum(1, 25);
         int sLength = (int) genRandomNum(1, 200);
 
         for (int i = 0; i < cases; i++) {
-            res = res + genString(orLength) + " " + genString(sLength) + '\n';
-            orLength = (int) genRandomNum(1, 26);
+            res = res + genString(orLength, true) + " " + genString(sLength, true) + '\n';
+            orLength = (int) genRandomNum(1, 25);
             sLength = (int) genRandomNum(1, 200);
         }
 
         return res;
     }
 
+    // Generate a random number between in range x,y
     public double genRandomNum(int min, int max) {
         double a = Math.random() * (max - min + 1) + min;
         return a;
     }
 
-    public String delDuplicates(String str) {
+    //Search if the number(character) is in the String
+    public boolean isIn(char s, String res){
+        boolean is = false;
+        for (int j = 0; j < res.length(); j++) {
+            if (s == res.charAt(j)) {
+                is = true;
+            }
+        }
+        return is;
+    }
+
+    // Generate a String with size (n)
+    // Receive true or false to generate a string with duplicate characters
+    public String genString(int size, boolean duplicates) {
         String res = "";
 
-        for (int i = 0; i < str.length(); i++) {
-            boolean found = true;
-            char k = str.charAt(i);
+        if (duplicates) {
+            char lett = (char) genRandomNum(97, 122);
+            res = res + lett;
             
-            for (int j = i; j < str.length() && found; j++) {
-                char p = str.charAt(j);
+            for (int i = 1; i < size; i++) {
+                lett = (char) genRandomNum(97, 122);
 
-                if (k == p) {
-                    res += p;
-                    found = false;
+                while (isIn(lett,res) && res.length()<= 25) {
+                    lett = (char) genRandomNum(97, 122);
                 }
+                res = res + lett;
+            }
+
+        } else {
+            for (int i = 0; i < size; i++) {
+                char lett = (char) genRandomNum(97, 122);
+                res = res + lett;
             }
         }
         return res;
     }
 
-    public String genString(int size) {
-        String res = "";
-
-        for (int i = 0; i < size; i++) {
-            char lett = (char) genRandomNum(97, 122);
-            res = res + lett;
-        }
-        System.out.print(res);
-        return res;
-    }
-
+    //Generate a txt in the selected path with all the test cases
     public void createDoc(String str) {
         String text = str;
         try {
--- Java/Main.java
+++ Java/Main.java
@@ -7,11 +7,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc  = new Scanner(System.in);
+        
         Gen g = new Gen();
-        // String letter = g.genData();
-        // g.createDoc(letter);
-        System.out.println(g.delDuplicates("aaaabbccc"));
-
+        g.createDoc(g.genData());
 
     }
 }
